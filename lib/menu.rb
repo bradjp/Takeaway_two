979488a0009a665bfcee2662005c72cd50f06224
Brad Phillips <bjrp@protonmail.com>
2020-03-14 16:32:03 +0000

    User story 1 complete

---
+++ lib/menu.rb
@@ -0,0 +1,16 @@
+class Menu
+
+  MENU = {'Margherita': 9.99, 'Chips': 1.99, 'Mozzarella sticks': 3.99,
+          'Hawaiian': 11.99, 'Garlic bread': 3.99}
+
+  def view_menu
+    format_menu()
+  end
+
+  private
+
+  def format_menu
+    MENU.map { |dish, price| "#{dish.to_s}: £#{price.to_s}" }.join("\n")
+  end
+
+end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,1 +1,14 @@
 require 'menu'
+
+describe Menu do
+  it 'displays a menu' do
+    expect(Menu::MENU[:Margherita]).to eq(9.99)
+  end
+end
+
+describe 'view_menu' do
+  it 'displays a menu' do
+    menu = Menu.new
+    expect(menu.view_menu()).to include('Margherita: £9.99')
+  end
+end
