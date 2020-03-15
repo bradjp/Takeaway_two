948961dae573b85b2755372e3d100301f07ee41f
Brad Phillips <bjrp@protonmail.com>
2020-03-15 16:20:01 +0000

    Refactors order spec tests

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,22 +1,26 @@
 require 'order'
 
 describe Order do
+
+  subject(:double) { described_class.new }
+
   describe '#add_item' do
     it 'adds an item to the order' do
-      order = Order.new
-      order.add_item('Margherita', 1)
-      expect(order.basket).to eq({'Margherita' => 1})
+      subject.add_item('Margherita', 1)
+      expect(subject.basket).to eq({'Margherita' => 1})
     end
     it 'adds more than one of an item to the order' do
-      order = Order.new
-      order.add_item('Margherita', 2)
-      expect(order.basket).to eq({'Margherita' => 2})
+      subject.add_item('Margherita', 2)
+      expect(subject.basket).to eq({'Margherita' => 2})
     end
     it 'adds an item to the order and updates the amounts' do
-      order = Order.new
-      order.add_item('Margherita', 1)
-      order.add_item('Margherita', 1)
-      expect(order.basket).to eq({'Margherita' => 2})
+      2.times { subject.add_item('Margherita', 1) }
+      expect(subject.basket).to eq({'Margherita' => 2})
+    end
+  end
+  describe '#remove_item' do
+    it 'removes items from the basket' do
+      
     end
   end
 end
